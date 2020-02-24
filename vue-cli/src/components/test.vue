68d7a5f426ef4e90be4da27b161ad8e17b88f33a
zaki1986 <zhang_chuang@nhn-st.com>
2020-02-24 16:29:56 +0800

    vue-cli

---
+++ vue-cli/src/components/test.vue
@@ -0,0 +1,25 @@
+<template>
+  <h2 class="test">this test vue {{msg}}</h2>
+</template>
+
+<script>
+export default {
+  name: 'testVue',
+  props:{
+    msg: {
+      type: String,
+      default: 'test msg'
+    }
+  },
+  methods:{
+
+  }
+}
+</script>
+
+<style scoped>
+  .test {
+    font-size: 40px;
+    color: gray;
+  }
+</style>
